69302162dae8cba14a59410c7e6ca7532c1df520
monicah79 <monniewanjema@gmail.com>
2023-07-18 01:43:27 +0300

    Requested changes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,30 +1,11 @@
 // Entry point for the build script in your package.json
 import '@hotwired/turbo-rails';
 import './controllers';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
-import Greeting from './Greeting';
-import reducer from './reducer';
-
-const store = configureStore({
-  reducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk),
-});
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Greeting />} />
-      </Routes>
-    </Router>
-  );
-}
+import App from './App';
+import store from './redux/configureStore';
 
 ReactDOM.render(
   <Provider store={store}>
